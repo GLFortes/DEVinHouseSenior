0f1b85057f1ddcd6e2e0803e0ba51610f2128840
Guilherme Fortes <guilhermelucianofortes@gmail.com>
2022-01-13 19:28:33 -0300

    Teste removendo arquivos

--- Exercicios_Semana_01/src/Aleatorio.java
+++
@@ -1,8 +0,0 @@
-// Ex1 - Questão aleatória
-// Crie um programa em Java que, ao executar, escreve na tela um número aleatório entre 0 e 1.
-public class Aleatorio {
-    public static void main(String[] args) {
-        double numero = Math.random();
-        System.out.println(numero);
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# DEVinHouseSenior
-Repositório com os códigos fonte dos exercícios solicitados no curso DEVinHouse
