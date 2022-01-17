802a76e57338b069f1a2d6665281018543834a30
Guilherme Fortes <59990084+GLFortes@users.noreply.github.com>
2022-01-17 10:05:31 -0300

    Delete Devinflix directory

--- Devinflix/src/usuarios/UsuarioAgregado.java
+++
@@ -1,7 +0,0 @@
-package usuarios;
-
-public class UsuarioAgregado extends Usuario {
-    public UsuarioAgregado(String nome, String endereco, String dataNascimento) {
-        super(nome, endereco, dataNascimento);
-    }
-}
--- Devinflix/src/usuarios/UsuarioMaster.java
+++
@@ -1,8 +0,0 @@
-package usuarios;
-
-public class UsuarioMaster extends Usuario {
-    private String senha;
-    public UsuarioMaster(String nome, String endereco, String dataNascimento) {
-        super(nome, endereco, dataNascimento);
-    }
-}
